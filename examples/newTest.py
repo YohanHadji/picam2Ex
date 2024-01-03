c5b3a505471d22d6c7c33b0ff4ce06c3e4379c7e
YohanHadji <hadji.yohan@gmail.com>
2024-01-03 15:51:30 +0100

    reduce average buffer

--- examples/newTest.py
+++ examples/newTest.py
@@ -41,7 +41,7 @@ class MovingAverageCalculator:
 
         return None  # Return None until the window is filled
 
-window_size = 1000
+window_size = 500
 fpsCalculator = MovingAverageCalculator(window_size)
 fpsDeviationCalculator = MovingAverageCalculator(window_size)
 fpsAverage = 0
