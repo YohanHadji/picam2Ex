a7e991ae71806d42ea9ad853ed4cccaf8393582f
YohanHadji <hadji.yohan@gmail.com>
2024-01-03 20:20:09 +0100

    revert newTest

--- examples/newTest.py
+++ examples/newTest.py
@@ -4,40 +4,22 @@ from picamera2 import Picamera2
 import time
 import random
 import string
-from collections import namedtuple
-import socket
 
-from capsule import *
-
-# Example of using the Capsule class
-class Foo:
-    pass
-
-def handle_packet(packetId, dataIn, len):
-    print(f"Received packet {packetId}: {dataIn[:len]}")
-
-capsule_instance = Capsule(lambda packetId, dataIn, len: handle_packet(packetId, dataIn, len))
-
-# Light point structure
-LightPoint = namedtuple('LightPoint', ['name','isVisible' 'x', 'y'])
-
-# Create an array of structures without specifying values
-LightPointArray = [LightPoint(None, None, None) for _ in range(10)]
-
-# Camera Init
+# Inicializar Picamera2
 picam2 = Picamera2()
+
+# Configuraciones de la cámara
 camera_config = picam2.create_video_configuration(main={"format": "BGR888", "size": (800, 606)}, raw={"format": "SRGGB10", "size": (1332, 990)})
 picam2.configure(camera_config)
 picam2.set_controls({"FrameRate": 120})
+# Iniciar la cámara
 picam2.start()
 
-# Global variables
 prev_time_sec = 0
 startTime = time.time()
 fpsCounter = 0
 all_light_points = []
 
-# Moving average calculator for fps measurement
 class MovingAverageCalculator:
     def __init__(self, window_size):
         self.window_size = window_size
@@ -65,9 +47,6 @@ fpsDeviationCalculator = MovingAverageCalculator(window_size)
 fpsAverage = 0
 fpsDeviation = 0
 previousMetadata = picam2.capture_metadata()
-
-teensyIP = "192.168.1.100"
-teensyPort = 8888
 
 def obtain_top_contours(b_frame, n=10):
     """
@@ -110,8 +89,14 @@ def is_point_close_with_motion_estimation(x1, y1, x2, y2, speed_x1, speed_y1, ac
     estimated_x = x1 + speed_x1 * delta_t + 0.5 * acceleration_x1 * delta_t/1e9**2
     estimated_y = y1 + speed_y1 * delta_t + 0.5 * acceleration_y1 * delta_t/1e9**2
 
+    # The threshold for each axis should be adjusted depending on the speed and acceleration of the object
+    thresholdx = 50
+    thresholdy = 50
+
+    # The allowed position range should be an ellipse with angle the speed vector angle and the major axis the norm of the speed vector and the minor axis a constant
+
     # Check if the new position is close to the estimated position
-    position_close = abs(x2 - estimated_x) <= threshold and abs(y2 - estimated_y) <= threshold
+    position_close = abs(x2 - estimated_x) <= thresholdx and abs(y2 - estimated_y) <= thresholdy
 
     return position_close
 
@@ -189,17 +174,17 @@ def process_and_store_light_points(new_points, sensorTimeStamp):
 
 
 while True:
-    # Get a frame with metadata
+    # Capturar el frame
     request = picam2.capture_request()
     frame = request.make_array("main")
+    #frame = picam2.capture_array("main")
     metadata = request.get_metadata()
     request.release()
 
     sensorTimeStamp = metadata['SensorTimestamp']
 
-    # Convert to grayscale and then to binary
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _dummy, b_frame = cv2.threshold(gray_frame,128, 255, cv2.THRESH_BINARY)
+    _dummy, b_frame = cv2.threshold(gray_frame,128, 255, cv2.THRESH_BINARY) ### gustavo
 
     result = obtain_top_contours(b_frame, 10)
     process_and_store_light_points(result, sensorTimeStamp)
@@ -208,8 +193,10 @@ while True:
     for i, (name, firstSeen, x, y, timestamp, speed_x, speed_y, acceleration_x, acceleration_y) in enumerate(all_light_points[:3]):
         print("Point %d: (%s, %d, %d, %d, %d, %d, %d)" % (i + 1, name, x, y, speed_x, speed_y, acceleration_x, acceleration_y))
 
-    # Display the frame rate
+    # Tiempo actual
     current_time_sec = time.time()
+
+    # Calcular el framerate
     if prev_time_sec != 0:
         fps = 1 / (current_time_sec - prev_time_sec)
         fpsAverage = fpsCalculator.calculate_moving_average(fps)
@@ -221,10 +208,10 @@ while True:
     prev_time_sec = current_time_sec
     previousMetadata = metadata
 
-    # Exit if 'q' is pressed
+    # Salir con 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release resources
+# Cerrar todas las ventanas
 cv2.destroyAllWindows()
 picam2.stop()
