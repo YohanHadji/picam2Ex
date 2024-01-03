9a63f69b8426a00b2a742666ebdabef743acea0f
YohanHadji <hadji.yohan@gmail.com>
2024-01-03 22:23:16 +0100

    fix sending part

--- examples/newCode/mainTracker.py
+++ examples/newCode/mainTracker.py
@@ -21,19 +21,23 @@ while True:
     pointToSend = getLockedPoint(joystickBtn, swUp, swDown, swLeft, swRight)
     sendTargetToTeensy()
 
-    arrayToSend = bytearray(10*13)
+    arrayToSend = bytearray(10*16)
     packet_id = 0x02
 
     # Fill light point array
     for i, (name, _, x, y, _, _, _, _, _) in enumerate(all_light_points[:10]):
-        byteToSend = struct.pack('4sbii'*10, LightPointArray)
-        payload_data = byteToSend
-        packet_length = len(payload_data)
-        encoded_packet = capsule_instance.encode(0x02, payload_data, packet_length)
-        # Convert encoded_packet to a bytearray
-        encoded_packet = bytearray(encoded_packet)
-        sock.sendto(encoded_packet, (OTHER_RASPI_IP, OTHER_RASPI_PORT))
+        pointToSend = LightPointArray[i]
+        byteToSend = struct.pack('4sbii', pointToSend.name.encode('utf-8'), pointToSend.isVisible, pointToSend.x, pointToSend.y)
+        # Concatenate the byte to the array
+        arrayToSend[i*16:(i+1)+16] = byteToSend
         #sock.sendto(encoded_packet, (GUSTAVO_IP, 8888))
+
+    payload_data = arrayToSend
+    packet_length = len(payload_data)
+    encoded_packet = capsule_instance.encode(0x02, payload_data, packet_length)
+    # Convert encoded_packet to a bytearray
+    encoded_packet = bytearray(encoded_packet)
+    sock.sendto(encoded_packet, (OTHER_RASPI_IP, OTHER_RASPI_PORT))
 
     #sendListToRaspi(LightPointArray)
 
