67cd7a58ab90538c66ec82021330e34afed93758
YohanHadji <hadji.yohan@gmail.com>
2024-01-05 19:56:23 +0100

    new example

--- examples/newCode/webserverDev/server.py
+++ examples/newCode/webserverDev/server.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Initialize a dictionary to store slider values
-slider_values = {}
+# Initialize a dictionary to store input values
+input_values = {}
 
 @app.route('/')
 def hello():
@@ -11,11 +11,18 @@ def hello():
 
 @app.route('/update_variable', methods=['POST'])
 def update_variable():
-    global slider_values
+    global input_values
     data = request.get_json()
-    slider_id = data['id']
-    slider_value = int(data['value'])
-    slider_values[slider_id] = slider_value
+    input_id = data['id']
+    input_value = int(data['value'])
+
+    # Check if the value has changed
+    if input_values.get(input_id) != input_value:
+        print(f"Value for {input_id} changed to {input_value}")
+
+    # Update the input_values dictionary
+    input_values[input_id] = input_value
+
     return "Variable updated successfully!"
 
 if __name__ == '__main__':
