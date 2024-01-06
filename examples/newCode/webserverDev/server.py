5d2b0e85136633dae0a7caf31ecbe4f6ba43ed13
YohanHadji <hadji.yohan@gmail.com>
2024-01-06 15:31:37 +0100

    simplify again

--- examples/newCode/webserverDev/server.py
+++ examples/newCode/webserverDev/server.py
@@ -11,36 +11,18 @@ app = Flask(__name__)
 
 camInit(30)
 
-# def udp_listener():
-#     UDP_IP = "0.0.0.0" 
-#     UDP_PORT = 8888
-
-#     sock = socket.socket(socket.AF_INET, # Internet
-#                          socket.SOCK_DGRAM) # UDP
-#     sock.bind((UDP_IP, UDP_PORT))
-
-#     while True:
-#         data, addr = sock.recvfrom(1024) 
-#         # Decode the data with capsule
-#         for byte in data:
-#             capsule_instance.decode(byte)
-
-
 def gen_frames():
 
     while True:
         # Capture the frame
         frame = picam2.capture_array()
-
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _dummy, b_frame = cv2.threshold(gray_frame,200, 255, cv2.THRESH_BINARY)
       
         # Encode the frame
-        _, buffer = cv2.imencode('.jpg', b_frame)
-        b_frame = buffer.tobytes()
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + b_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -52,11 +34,8 @@ def index():
     # HTML
     return render_template('index.html')
 
-
 if __name__ == '__main__':
     try:
-        # udp_thread = threading.Thread(target=udp_listener)
-        # udp_thread.start()
         app.run(host='0.0.0.0', port=5000)
     finally:
         picam2.stop()
